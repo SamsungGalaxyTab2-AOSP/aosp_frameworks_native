LineageOS/android_frameworks_native

libbinder: allow devices to disable ashmem size tracking
The addition of ashmem size tracking can lead to parcel objects
overwriting other values on the stack in old binary blobs.
At the very least, this fixes RIL on i9300.

Change-Id: Ida52cec851a6f9d5a57c8f9130a5875c03dcb094

include/binder/Parcel.h
@@ -340,8 +340,10 @@ public:
         inline void* data() { return mData; }
     };
 
+#ifndef DISABLE_ASHMEM
 private:
     size_t mBlobAshmemSize;
+#endif
 
 public:
     size_t getBlobAshmemSize() const;

SamsungGalaxyTab2-AOSP/aosp_frameworks_native

Change-Id: Ife8514be1ba639c4061de38b59794c46bcc2d7f8

include/binder/Parcel.h
@@ -340,8 +340,10 @@ public:
         inline void* data() { return mData; }
     };
 
+#ifndef DISABLE_ASHMEM_TRACKING
 private:
     size_t mOpenAshmemSize;
+#endif
 
 public:
     // TODO: Remove once ABI can be changed.